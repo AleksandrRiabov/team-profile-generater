a31d2adf72acd3d7bdf7c8ac9a92f4dd438240b8
Aleksandr Riabov <sania.riabov@gmail.com>
2023-02-24 03:48:11 +0000

    Initial Managers quetions prompted

--- index.js
+++ index.js
@@ -13,3 +13,72 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+inquirer.prompt(
+  [
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'name',
+      validate: validateInput
+    },
+    {
+      type: 'input',
+      message: 'What is Employee ID',
+      name: 'emloyeeID',
+      validate: validateInput
+    },
+    {
+      type: 'email',
+      message: `Please enter user's email`,
+      name: 'email',
+      default: () => { },
+      validate: validateEmail
+    },
+    {
+      type: 'input',
+      message: 'What is your Office Number?',
+      name: 'officeNumber',
+      validate: validateNumber
+    },
+    {
+      type: 'list',
+      message: 'What is your Office Number?',
+      choices: [new inquirer.Separator(),'Add engineer', new inquirer.Separator(),'Add Intern', new inquirer.Separator(),'Finish building the team',new inquirer.Separator()],
+      name: 'addEmployee',
+    },
+  ]
+)
+  .then(response => {
+    console.log(response)
+  })
+
+
+
+function validateEmail(email) {
+  const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+  if (valid) {
+    return true;
+  } else {
+    console.log(".  Please enter a valid email");
+    return false;
+  }
+}
+
+function validateInput(val) {
+  if (val.trim()) {
+    return true
+  } else {
+    console.log(' Invalid Input.');
+    return false
+  }
+}
+
+function validateNumber (val) {
+  if (parseInt(val.trim())) {
+    return true
+  } else {
+    console.log(' Must be a Number');
+    return false
+  }
+}
